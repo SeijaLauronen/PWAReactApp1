f888683775eff23c6025cf47233d7b259c22b5b6
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2024-06-09 21:31:36 +0300

    Lisätty basic styled component button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import StyledButton from './components/Button/Button'
 
 function App() {
   return (
     <div className="App">
       <p>ReactPWA 2 ServiceWorker</p>
+      <p>
+      <button>Button</button>
+      </p>
+      <StyledButton>Styled Button</StyledButton>
     </div>
   );
 }
