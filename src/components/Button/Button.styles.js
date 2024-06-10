e20a7d7bc436b40a8769508a23782504147e5bdb
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2024-06-10 16:28:23 +0300

    Välitetään Props ja lisätään Attributes

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -26,3 +26,17 @@ export const FancyButton = styled(StyledButton)`
 background-image: linear-gradient(to right, #f6d365 0% , #fda085 100% );
 border:none;
 `
+
+/*export const SubmitButton = styled(StyledButton).attrs({*/
+/* Yllä oleva toimii ok, kun tässä attrs osassa ei käytetä propsia, jos käytettäisiiin, tulisi se näin:*/
+export const SubmitButton = styled(StyledButton).attrs((props)=>({
+    type: 'submit',
+}))` /* ja tästä sulku pois jos ei propsia*/
+    box-shadow: 0 9px #999;
+    &:active {
+        background-color: ${(props) => 
+        props.variant !=='outline' ? '#FFF' : '#4caf50'};
+        box-shadow: 0 5px #666;
+        transform:translateY(4px)
+    }
+`
