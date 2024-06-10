fffa86c06331cd66e8372d6bc80f852cc14e90f3
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2024-06-10 15:59:25 +0300

    Extended style ja polymofismi as linkkinä

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 //Jostain syystä valittaa tästä, että on jo importattu, mutta pienellä kirjaimella button
-import StyledButton from './components/Button/Button'
+import StyledButton, {FancyButton} from './components/Button/Button'
 
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
       <StyledButton>Styled Button</StyledButton>
       <div><br/></div>
       <StyledButton variant='outline'>Styled Button</StyledButton>
+      <div><br/></div>
+      <FancyButton as='a'>Fancy Button, linkkinä</FancyButton>
     </div>
   );
 }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,2 +1,3 @@
-import { StyledButton } from "./Button.styles";
+import { StyledButton, FancyButton } from "./Button.styles";
 export default StyledButton;
+export {FancyButton};
